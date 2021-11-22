72326985192b98c77f2e7bcc4cbf9fcb61e935f6
hotlovestl <pluhin34@gmail.com>
2021-11-23 02:41:16 +0300

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,3 +1,20 @@
 import { showTime } from "./time.js";
+import { showGreeting } from "./greeting.js";
+import name from "./greeting.js";
+
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+
+window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('load', getLocalStorage);
+name.addEventListener('change', setLocalStorage);
 
 showTime();
+showGreeting();
